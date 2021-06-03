9fba19ff0dfa0071289bb13f72e7bdb9cd41b003
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2021-06-03 22:16:59 +0600

    perfectly show chart in UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Chart from '../components/chart/Chart'
 import FeatureInfo from '../components/featureInfo/FeatureInfo'
-import "./Home.css"
+import "./Home.css";
+import { userData } from "../../src/dummyData"
+
 export default function Home() {
     return (
         <div className="home">
             <FeatureInfo />
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
         </div>
     )
 }
